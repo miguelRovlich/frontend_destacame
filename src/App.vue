33a3099340bc685f1fd4cab46bca3b74926fa7f1
Alejandro Méndez <alejandro.mendez.developer@gmail.com>
2022-03-01 13:50:20 -0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
+<!--<template>-->
+  <!--<div id="app">-->
+    <!--<nav>-->
+      <!--<router-link to="/">Aca</router-link> |-->
+      <!--<router-link to="/about">About</router-link>-->
+    <!--</nav>-->
+    <!--<router-view/>-->
+  <!--</div>-->
+<!--</template>-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app :dark="goDark">
+    <v-content>
+      <v-container align-center>
+        <TheHeader :goDark="goDark" @changeTheme="updateTheme($event)"/>
+
+        <transition
+          name="router-animation"
+          mode="out-in"
+          enter-active-class="animated fadeIn fast"
+          leave-active-class="animated fadeOut faster"
+        >
+          <router-view></router-view>
+        </transition>
+      </v-container>
+    </v-content>
+    <TheFooter/>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
+@import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
